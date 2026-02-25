e83113f915a9c1beadde81a459a76d3af5daf329
ayoub houdda <houddaayoub34@gmail.com>
2026-02-25 10:18:45 +0000

    L'ajout du migration et model pour les categories

--- src/database/migrations/2026_02_25_100838_create_categories_table.php
+++ src/database/migrations/2026_02_25_100838_create_categories_table.php
@@ -14,6 +14,10 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name');
+            $table->foreignId('colocation_id')->constrained()->onDelete('cascade');
+            // on utilise un tableau [] pour coupler les colonnes
+            $table->unique(['name', 'colocation_id']);
         });
     }
 
